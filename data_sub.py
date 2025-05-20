fa8f3fda4b4bd966e86d230d7fab861a38de92c2
Delanie <degraham@pdx.edu>
2025-05-19 17:39:29 -0700

    Removed commented out code. Removed SQL query to add to Trip table, implemented copy_from. data_sub.py now runs on an infinite loop. Removed DB password and added to .env. Validation and transformations all include try/assert/except blocks within separate functions. Removed nested function in valid.py.

--- data_sub.py
+++ data_sub.py
@@ -2,12 +2,14 @@ from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
 from datetime import datetime, timedelta
 from valid import valid_and_trans
+from dotenv import load_dotenv
 
 import threading
 import json
 import psycopg2
 import io
 import pandas as pd
+import os
 
 timestamp = datetime.now()
 formatted_timestamp = timestamp.strftime('%Y-%m-%d_%H:%M:%S')
@@ -43,8 +45,6 @@ def callback(message: pubsub_v1.subscriber.message.Message) -> None:
         count += 1
         message_json = json.loads(message_data)
         messages.append(message_json)
-        #with open(f"{date}.json", "a") as file:
-        #    file.write(json.dumps(message_json) + "\n") 
     message.ack()
 
 def db_connect():
@@ -52,7 +52,7 @@ def db_connect():
         host="localhost",
         database="postgres",
         user="postgres",
-        password="postgres",
+        password=os.environ.get("PASSWORD"),
     )
     connection.autocommit = True
     return connection
@@ -60,22 +60,15 @@ def db_connect():
 def load(conn, df):
 
     trip = df[['EVENT_NO_TRIP', 'VEHICLE_ID']].drop_duplicates(subset='EVENT_NO_TRIP')
+    trip.rename(columns={'EVENT_NO_TRIP': 'trip_id', 'VEHICLE_ID': 'vehicle_id'}, inplace=True)
     bc = df[['TIMESTAMP', 'GPS_LATITUDE', 'GPS_LONGITUDE', 'SPEED', 'EVENT_NO_TRIP']]
     with conn.cursor() as cursor:
         try:
-            
-            # use sql insert to insert into trip table
-            for _, row in trip.iterrows():
-                trip_id = int(row['EVENT_NO_TRIP'])
-                vehicle_id = int(row['VEHICLE_ID'])
-                cursor.execute(
-                    """
-                    INSERT INTO Trip (trip_id, vehicle_id) 
-                    VALUES (%s, %s) 
-                    ON CONFLICT (trip_id) DO NOTHING;
-                    """,
-                    (trip_id, vehicle_id))
-            # use copy_from to bulk insert into breadcrumb
+            csv_trip = trip.to_csv(index=False)
+            f_trip = io.StringIO(csv)
+            next(f_trip)
+            cursor.copy_from(f_trip, 'trip', sep=',', columns=['trip_id', 'vehicle_id'])
+
             csv = bc.to_csv(index=False)
             f = io.StringIO(csv)
             next(f)
@@ -89,14 +82,10 @@ def main():
     streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
 
     with subscriber:
-        try:
-            while True:
+        while True:
         # When `timeout` is not set, result() will block indefinitely,
         # unless an exception is encountered first.
                 streaming_pull_future.result(timeout=timeout)
-        except TimeoutError:
-            streaming_pull_future.cancel()  # Trigger the shutdown.
-            streaming_pull_future.result()  # Block until the shutdown is complete.
 
     if count > 0:
         with open("sub_log.txt", "a") as file:
@@ -108,7 +97,11 @@ def main():
     
     #Connect to db and put in db
     conn = db_connect()
-    load(conn, df)
+    if df.empty:
+        with open("db_err.txt", "a") as file:
+            file.write("Empty dataframe.")
+    else:
+        load(conn, df)
 
 if __name__ == "__main__":
     main()
--- valid.py
+++ valid.py
@@ -1,41 +1,76 @@
 import pandas as pd
 
+def write_error(e):
+    with open("validation_err.txt", "a") as file:
+        file.write(e)
 
-def valid_and_trans(messages):
-    #df = pd.read_json(messages)
-    #df = pd.DataFrame(messages)
-  
-    if messages.empty or messages.shape[1] == 0:
-        print("Warning: Received empty DataFrame (0, 0), skipping validation.")
-        return pd.DataFrame()
-    
-    df = messages.copy()
-    df = df.drop(columns=["EVENT_NO_STOP", "GPS_SATELLITES", "GPS_HDOP"])
+def empty_df(df):
+    try:
+        assert not df.empty, "DF is empty"
+        assert not df.shape[1] == 0, "DF has no rows"
+    except AssertionError as e:
+        write_error(e)
 
+def drop_cols(df):
+    try:
+        df = df.drop(columns=["EVENT_NO_STOP", "GPS_SATELLITES", "GPS_HDOP"])
+        required_cols =  {"OPD_DATE", "ACT_TIME", "GPS_LATITUDE", 
+                          "GPS_LONGITUDE", "SPEED", "EVENT_NO_TRIP"}
+        assert required_cols.issubset(df.columns), f"Columns present: {set(df.columns)}"
+    except AssertionError as e:
+        write_error(e)
+    return df
+
+def neg_meters(df):
+    try:
     # 1 Limit - METERS cannot be negative, it is a distance measurement
-    df = df[df["METERS"] >= 0]
+        df = df[df["METERS"] >= 0]
+        assert (df["METERS"] < 0).sum() == 0, "Negative meters still present."
+    except AssertionError as e:
+        write_error(e)
+    return df
 
+def neg_time(df):
+    try:
     # 2 Limit - ACT_TIME cannot be negative, time measurement
-    df = df[df["ACT_TIME"] >= 0]
+        df = df[df["ACT_TIME"] >= 0]
+        assert (df["ACT_TIME"] < 0).sum() == 0, "Negative time still present."
+    except AssertionError as e:
+        write_error(e)
+    return df
 
-
+def timestamp_col(df):
     # 3 Existence - TIMESTAMP column with date and time
     # Transform OPD_DATE and ACT_TIME to TIMESTAMP column
-    df["OPD_DATE"] = df["OPD_DATE"].str.split(':').str[0]
-    df["TIMESTAMP"] = (pd.to_datetime(df["OPD_DATE"]) + pd.to_timedelta(df["ACT_TIME"], unit='s'))
-    df = df.drop(columns=['ACT_TIME', 'OPD_DATE'])
+    try:
+        df["OPD_DATE"] = df["OPD_DATE"].str.split(':').str[0]
+        df["TIMESTAMP"] = (pd.to_datetime(df["OPD_DATE"]) + pd.to_timedelta(df["ACT_TIME"], unit='s'))
+        df = df.drop(columns=['ACT_TIME', 'OPD_DATE'])
+        df = df.sort_values(by=["TIMESTAMP", "EVENT_NO_TRIP"])
+        assert 'TIMESTAMP' in df.columns, "TIMESTAMP column not created"
+    except AssertionError as e:
+        write_error(e)
+    return df
 
-
-    df = df.sort_values(by=["TIMESTAMP", "EVENT_NO_TRIP"])
-
+def trip_exist(df):
     # 4 Existence - Every record must have a EVENT_NO_TRIP
     # Drop remaining rows where EVENT_NO_TRIP is still missing
-    df.dropna(subset=["EVENT_NO_TRIP"], inplace=True)
+    try:
+        df.dropna(subset=["EVENT_NO_TRIP"], inplace=True)
+        assert not df['EVENT_NO_TRIP'].isna().any(), "Null values in EVENT_NO_TRIP"
+    except AssertionError as e:
+        write_error(e)
+    return df
 
+def vehicle_id(df):
     # 5 Referential Integerity - VEHICLE_ID must be within Team1's assignment
-    bus = pd.read_csv("vehicleGroupsIds.csv")
-    df = df[df["VEHICLE_ID"].isin(bus["Whisker"])]
-
+    try:
+        bus = pd.read_csv("vehicleGroupsIds.csv")
+        df = df[df["VEHICLE_ID"].isin(bus["Whisker"])]
+        assert df["VEHICLE_ID"].isin(bus["Whisker"]).all(), "Extraneous vehicle ids."
+    except AssertionError as e:
+        write_error(e)
+    return df
 
     # Transform Meters and Timestamp into Speed column
     # 6 Intra-Record Validation - Speed is determined based on delta 
@@ -46,35 +81,76 @@ def valid_and_trans(messages):
     # Transform empty speed cell with next valid value. 
     # Speed cannot be over 70 mph
     # Speed cannot be NaN
-    def trip_speed(group):
-        group = group.sort_values("TIMESTAMP")
-        group['dMETERS'] = group['METERS'].diff()
-        group['dTIME'] = group['TIMESTAMP'].diff().dt.total_seconds()
-        group['SPEED'] = group['dMETERS'] / group['dTIME']
-        group['SPEED'] = group['SPEED'].bfill()
-        group = group.drop(columns=["dMETERS", "dTIME"])
-        group = group.dropna(subset=['SPEED'])
-        group = group[group['SPEED'] <= 70]
-        return group
 
+def trip_speed(group):
+    group = group.sort_values("TIMESTAMP")
+    group['dMETERS'] = group['METERS'].diff()
+    group['dTIME'] = group['TIMESTAMP'].diff().dt.total_seconds()
+    group['SPEED'] = group['dMETERS'] / group['dTIME']
+    group['SPEED'] = group['SPEED'].bfill()
+    group = group.drop(columns=["dMETERS", "dTIME"])
+    group = group.dropna(subset=['SPEED'])
+    group = group[group['SPEED'] <= 70]
+    return group
 
-    
-    df = df.groupby(by="EVENT_NO_TRIP", group_keys=False).apply(trip_speed).reset_index(drop=True)
+def speed_wrapper(df):
+    try:
+        df = df.groupby(by="EVENT_NO_TRIP", group_keys=False).apply(trip_speed).reset_index(drop=True)
+        assert 'SPEED' in df.columns, "Missing SPEED column."
+    except AssertionError as e:
+        write_error(e)
+    return df
 
+def longitude(df):
     # 8 Existence - Longitude exists
-    df = df.dropna(subset=["GPS_LONGITUDE"])
+    try:
+        df = df.dropna(subset=["GPS_LONGITUDE"])
+        assert not df['GPS_LONGITUDE'].isna().any(), "null GPS_LONGITUDE still exists."
+    except AssertionError as e:
+        write_error(e)
+    return df
 
+def latitude(df):
     # 9 Existence - Latitude exists
-    df = df.dropna(subset=["GPS_LATITUDE"])
+    try:
+        df = df.dropna(subset=["GPS_LATITUDE"])
+        assert not df['GPS_LATITUDE'].isna().any(), "null GPS_LATITUDE still exists."
+    except AssertionError as e:
+        write_error(e)
+    return df
 
+def avg_speed(df):
     # 10 Statistical - Average speed is less than 60 miles per hour
-    if df['SPEED'].mean() > 60:
-        print("Average speed was greater than 60 mph")
+    try:
+        assert df['SPEED'].mean() <= 60, "Average speed was greater than 60 mph"
+    except AssertionError as e:
+        write_error(e)
 
+def column_organization(df):
     # Reorganize columns for copy_from
-    
-    df = df[['TIMESTAMP', 'GPS_LATITUDE', 'GPS_LONGITUDE', 'SPEED', 'EVENT_NO_TRIP', 'VEHICLE_ID']]
-    
+    try:
+        expected_cols = ['TIMESTAMP', 'GPS_LATITUDE', 'GPS_LONGITUDE', 'SPEED', 'EVENT_NO_TRIP', 'VEHICLE_ID']
+        df = df[expected_cols]
+        assert expected_cols.issubset(df.columns), f"Columns present: {set(df.columns)}"
+    except AssertionError as e:
+        write_error(e)
     return(df)
 
+
+def valid_and_trans(messages):
+    #check for empty df
+    empty_df(messages)
+    df = messages.copy()
+
+    df = drop_cols(df)      
+    df = neg_meters(df)
+    df = neg_time(df)
+    df = timestamp_col(df)
+    df = trip_exist(df)
+    df = vehicle_id(df)
+    df = speed_wrapper(df)
+    df = longitude(df)
+    df = latitude(df)
+    avg_speed(df)
+    df = column_organization(df)
 
